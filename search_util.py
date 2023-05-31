fd9baa04b2886f57e6d5c8caf05ac1aa66b8d4f3
pnadelofficial <75741790+pnadelofficial@users.noreply.github.com>
2023-05-31 15:19:18 -0400

    Update search_util.py

--- search_util.py
+++ search_util.py
@@ -14,50 +14,51 @@ import subprocess
 @st.cache_resource
 def get_data():
     #simple
-    statements_url = 'https://drive.google.com/drive/folders/1HC90kBcGI_0fT25ao0b_BlWTpE2e_CP6?usp=sharing&confirm=t'
+    ## statments
+    statements_url = 'https://drive.google.com/drive/folders/1kJLVteheTQcONEocW4gWlhgC5EOm_4qP?usp=sharing'
     gdown.download_folder(statements_url, quiet=True, use_cookies=False)
     ## transcripts
-    transcript_url = 'https://drive.google.com/drive/folders/1LIl8jfzfrQQD6wkOYpvM0osYSt3dGog_?usp=sharing&confirm=t'
+    transcript_url = 'https://drive.google.com/drive/folders/1FDezT_xPLF0FxOOcnXoYgDGvJpMPwpfa?usp=sharing'
     gdown.download_folder(transcript_url, quiet=True, use_cookies=False)
 
     #semantic
-    model_url = 'https://drive.google.com/drive/folders/1iqutHw9dJGqSnRBgrdvegmWEJ-4najWN?usp=sharing&confirm=t'
+    model_url = 'https://drive.google.com/drive/folders/1iqutHw9dJGqSnRBgrdvegmWEJ-4najWN?usp=sharing'
     gdown.download_folder(model_url, quiet=True, use_cookies=False)
     ss_model = SentenceTransformer('npi_ft542023_L12')
     ## transcripts
-    transcript_sem_search_url = 'https://drive.google.com/drive/folders/1u5YABhLt-wv0iur-WSDR61uc7bhEkVbg?usp=sharing&confirm=t'
+    transcript_sem_search_url = 'https://drive.google.com/drive/folders/1u5YABhLt-wv0iur-WSDR61uc7bhEkVbg?usp=sharing'
     gdown.download_folder(transcript_sem_search_url, quiet=True, use_cookies=False)
     t_embeddings = Embeddings()
     t_embeddings.load("npi_transcript_sem_search_index_d")
     ## statements
-    ws_sem_search_url = 'https://drive.google.com/drive/folders/12Ym7Ya1C1mzIlVvvOrv42pE3rV2dsf4j?usp=sharing&confirm=t'
+    ws_sem_search_url = 'https://drive.google.com/drive/folders/12Ym7Ya1C1mzIlVvvOrv42pE3rV2dsf4j?usp=sharing'
     gdown.download_folder(ws_sem_search_url, quiet=True, use_cookies=False)
     w_embeddings = Embeddings()
     w_embeddings.load("npi_ws_sem_search_index_d")
 
     #org data
     ## transcripts
-    at_url = 'https://drive.google.com/file/d/1Uxqd_SKwwzzPmNdnauDmJvdzZUk2Oo0b/view?usp=sharing&confirm=t'
+    at_url = 'https://drive.google.com/file/d/1UKX7rl7TI3QfXdInvtS6uyCMv7DMbrAc/view?usp=sharing'
     t_output = 'all_transcripts_d.csv'
     gdown.download(at_url, t_output, quiet=True, fuzzy=True)
     transcripts = pd.read_csv('all_transcripts_d.csv').rename(columns={'index':'org_index'}).dropna()
     ## statements
-    ast_url = 'https://drive.google.com/file/d/1UOdw6SsoGbMFWAFuUnaJ3yZoJvFpWlAF/view?usp=sharing&confirm=t'
+    ast_url = 'https://drive.google.com/file/d/1MmKY3P4tUMP6IQkdBy6Eo8yGpnubTjBY/view?usp=sharing'
     w_output = 'all_written_statements_d.csv'
     gdown.download(ast_url, w_output, quiet=True, fuzzy=True)
     written_statements = pd.read_csv('all_written_statements_d.csv').rename(columns={'index':'org_index'}).dropna()
 
     #reference data
     ## transcripts
-    t_ref_url = 'https://drive.google.com/file/d/1qJZR3lI95lrMCPATQ8xzkbL9_tQVmT7d/view?usp=sharing&confirm=t'
+    t_ref_url = 'https://drive.google.com/file/d/1F9xLAVeiMmxb4dsdVMseT1BXo629xjz2/view?usp=sharing'
     t_ref_output = 'transcript_reference_d.csv'
     gdown.download(t_ref_url, t_ref_output, quiet=True, fuzzy=True)
     t_ref = pd.read_csv('transcript_reference_d.csv')
     ## statements
-    w_ref_url = 'https://drive.google.com/file/d/1ysGvJKEqD12SNMOUFmhqAG46hPThV51V/view?usp=sharing&confirm=t'
+    w_ref_url = 'https://drive.google.com/file/d/1WYsv6n-2hIlvE7GdziEmmg8ARNfc9VSA/view?usp=sharing'
     w_ref_output = 'ws_reference_d.csv'
     gdown.download(w_ref_url, w_ref_output, quiet=True, fuzzy=True)
-    w_ref = pd.read_csv('ws_reference_d.csv').rename(columns={'fname':'filename'})
+    w_ref = pd.read_csv('ws_reference_d.csv')
 
     # for sentiment analysis
 #     model_path = "cardiffnlp/twitter-xlm-roberta-base-sentiment"
